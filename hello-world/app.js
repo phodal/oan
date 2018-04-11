2a4a95e18df584608383e77d00f7d8a577e66707
Phodal HUANG <h@phodal.com>
2018-04-11 22:36:52 +0800

    [T] create pure hello world

---
+++ hello-world/app.js
@@ -0,0 +1,19 @@
+class HelloElement extends HTMLElement {
+    constructor() {
+        super();
+        var shadow = this.attachShadow({mode: 'open'});
+        shadow.innerHTML += `
+		<style>
+			#tabs { background-color: red; }
+			span { color: purple; }
+		</style>
+		<div id="tabs">
+			hello
+		</div>
+		<span>there</span>
+		`;
+    }
+}
+
+// Define the new element with the CustomElementsRegistry
+customElements.define('hello-element', HelloElement);
