127fb6a5aec120259f28867e49734eb60a6ec4d9
Phodal HUANG <h@phodal.com>
2018-04-13 13:23:34 +0800

    [T] thinking in api design

--- hello-world-dili/app.js
+++ hello-world-dili/app.js
@@ -12,6 +12,14 @@
 
 Component({
   is: 'di-li',
+  data: {
+    text: 'Hello, World'
+  },
+  methods: {
+    world: function() {
+      console.log('world');
+    }
+  },
   connected: function() {
     console.log("di-li connectedCallback")
   }
