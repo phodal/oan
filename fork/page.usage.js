0f8a189ddc83601bdb87c381f1dd78a990514694
Phodal HUANG <h@phodal.com>
2018-04-18 08:57:50 +0800

    [T] thinking in page.js

---
+++ fork/page.usage.js
@@ -0,0 +1,43 @@
+Page({
+  data: {
+
+  },
+  onLoad: function(options) {
+    // Do some initialize when page load.
+  },
+  onReady: function() {
+    // Do something when page ready.
+  },
+  onShow: function() {
+    // Do something when page show.
+  },
+  onHide: function() {
+    // Do something when page hide.
+  },
+  onUnload: function() {
+    // Do something when page close.
+  },
+  onPullDownRefresh: function() {
+    // Do something when pull down.
+  },
+  onReachBottom: function() {
+    // Do something when page reach bottom.
+  },
+  onShareAppMessage: function () {
+    // return custom share data when user share.
+  },
+  onPageScroll: function() {
+    // Do something when page scroll
+  },
+  onTabItemTap(item) {
+    console.log(item.index)
+    console.log(item.pagePath)
+    console.log(item.text)
+  },
+  viewTap: function() {
+
+  },
+  customData: {
+    hi: 'MINA'
+  }
+})
