8b0f2bf33854c21cd1ee68f6f4c73e7350f5e21f
Phodal HUANG <h@phodal.com>
2018-04-18 09:01:47 +0800

    [TWB] thinking in obser

--- dili.js
+++ dili.js
@@ -1,5 +1,19 @@
 let appConfig = {};
 const components = [];
+// https://leihuang.me/posts/implementing-two-way-data-binding-with-vanilla-javascript/
+const observable = value => {
+  const listeners = [];
+  const notify = newVal => listeners.forEach(listener => listener(newVal))
+  function observer(newValue) {
+    if (arguments.length && newValue !== value) {
+      value = newValue;
+      notify(newValue);
+    }
+    return value;
+  }
+  observer.subscribe = function(listener) { listeners.push(listener); };
+  return observer;
+}
 
 function createClass(component) {
   var klass;
