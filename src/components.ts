5d90a737ffe69af8bbe34005ea9f05fad83a6eb4
Phodal HUANG <h@phodal.com>
2018-04-26 08:53:47 +0800

    [T] typescript not work

--- src/components.ts
+++ src/components.ts
@@ -1,55 +1,56 @@
-function createClass(component: any) {
-  var klass;
-  klass = class extends HTMLElement {
-    constructor() {
-      super();
+class BaseElement extends HTMLElement {
+  private component: any;
+  constructor(component: any) {
+    super();
 
-      if (this.attributes.length > 0) {
-        console.log(this.getAttribute(this.attributes[0].name));
-      }
-      let templateKeys = this.getTemplateKey(this.textContent);
-      this.textContent = this.renderText(this.textContent, templateKeys, component.data);
+    this.component = component;
+    if (this.attributes.length > 0) {
+      console.log(this.getAttribute(this.attributes[0].name));
     }
+    let templateKeys = this.getTemplateKey(this.textContent);
+    this.textContent = this.renderText(this.textContent, templateKeys, component.data);
+  }
 
-    renderText(content: any, templateKeys: any, data: any) {
-      for (let i = 0; i < templateKeys.length; i ++) {
-        let key = templateKeys[i];
-        if (data.hasOwnProperty(key)) {
-          let searchValue = `{{${key}}}`;
-          content = content.replace(new RegExp(searchValue, 'g'), data[key]);
-        } else {
-          if (/{{.*}}/.test(content)) {
-            console.warn('template,', /{{.*}}/.exec(content), 'not found')
-          }
-          content = content.replace(/{{.*}}/, '');
+  renderText(content: any, templateKeys: any, data: any) {
+    for (let i = 0; i < templateKeys.length; i ++) {
+      let key = templateKeys[i];
+      if (data.hasOwnProperty(key)) {
+        let searchValue = `{{${key}}}`;
+        content = content.replace(new RegExp(searchValue, 'g'), data[key]);
+      } else {
+        if (/{{.*}}/.test(content)) {
+          console.warn('template,', /{{.*}}/.exec(content), 'not found')
         }
+        content = content.replace(/{{.*}}/, '');
       }
-
-      return content;
     }
 
-    getTemplateKey(str: any) {
-      return str.match(/{{\s*[\w\.]+\s*}}/g).map((x: any) => x.match(/[\w\.]+/)[0]);
-    }
+    return content;
+  }
 
-    connectedCallback() {
-      component.connected()
-    }
+  getTemplateKey(str: any) {
+    return str.match(/{{\s*[\w\.]+\s*}}/g).map((x: any) => x.match(/[\w\.]+/)[0]);
+  }
 
-    attributeChangedCallback(attr: any, oldVal: any, newVal: any) {
-      console.log(attr)
-    }
+  connectedCallback() {
+    this.component.connected()
+  }
 
-    disconnectedCallback() {
+  attributeChangedCallback(attr: any, oldVal: any, newVal: any) {
+    console.log(attr)
+  }
 
-    }
+  disconnectedCallback() {
 
-    adoptedCallback() {
+  }
 
-    }
-  };
+  adoptedCallback() {
 
-  return klass
+  }
+}
+
+function createClass(component: any) {
+  return new BaseElement(component)
 }
 
 export class DiliComponent {
@@ -59,6 +60,6 @@ export class DiliComponent {
   }
 
   init(component: any) {
-    customElements.define(`${component.is}`, createClass(component));
+    customElements.define(`${component.is}`, createClass);
   }
 }
