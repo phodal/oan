9102989a3203a8f5269a9cf51eecdfb1dc2fd013
Phodal HUANG <h@phodal.com>
2018-05-28 09:21:09 +0800

    [T] it works

--- src/components.ts
+++ src/components.ts
@@ -1,59 +1,3 @@
-class BaseElement extends HTMLElement {
-  private component: any
-
-  constructor(component: any) {
-    super()
-
-    this.component = component
-    if (this.attributes.length > 0) {
-      console.log(this.getAttribute(this.attributes[0].name))
-    }
-    let templateKeys = this.getTemplateKey(this.textContent)
-    this.textContent = this.renderText(this.textContent, templateKeys, component.data)
-  }
-
-  renderText(content: any, templateKeys: any, data: any) {
-    for (let i = 0; i < templateKeys.length; i++) {
-      let key = templateKeys[i]
-      if (data.hasOwnProperty(key)) {
-        let searchValue = `{{${key}}}`
-        content = content.replace(new RegExp(searchValue, 'g'), data[key])
-      } else {
-        if (/{{.*}}/.test(content)) {
-          console.warn('template,', /{{.*}}/.exec(content), 'not found')
-        }
-        content = content.replace(/{{.*}}/, '')
-      }
-    }
-
-    return content
-  }
-
-  getTemplateKey(str: any) {
-    return str.match(/{{\s*[\w\.]+\s*}}/g).map((x: any) => x.match(/[\w\.]+/)[0])
-  }
-
-  connectedCallback() {
-    this.component.connected()
-  }
-
-  attributeChangedCallback(attr: any, oldVal: any, newVal: any) {
-    console.log(attr)
-  }
-
-  disconnectedCallback() {
-    return
-  }
-
-  adoptedCallback() {
-    return
-  }
-}
-
-function createClass(component: any) {
-  return new BaseElement(component)
-}
-
 export class DiliComponent {
   constructor(component: any) {
     this.init(component)
@@ -61,6 +5,54 @@ export class DiliComponent {
   }
 
   init(component: any) {
-    customElements.define(`${component.is}`, createClass)
+    function DiliElement() {
+      return Reflect.construct(HTMLElement, [], DiliElement)
+    }
+
+    DiliElement.prototype = Object.create(HTMLElement.prototype, {
+      constructor: {
+        value: function constructor() {
+          console.log(this.component)
+          return DiliElement
+        }
+      },
+
+      getTemplateKey: {
+        value: function getTemplateKey(str: any) {
+          return str.match(/{{\s*[\w\.]+\s*}}/g).map((x: any) => x.match(/[\w\.]+/)[0])
+        }
+      },
+      renderText: {
+        value: function renderText(content: any, templateKeys: any, data: any) {
+          for (let i = 0; i < templateKeys.length; i++) {
+            let key = templateKeys[i]
+            if (data.hasOwnProperty(key)) {
+              let searchValue = `{{${key}}}`
+              content = content.replace(new RegExp(searchValue, 'g'), data[key])
+            } else {
+              if (/{{.*}}/.test(content)) {
+                console.warn('template,', /{{.*}}/.exec(content), 'not found')
+              }
+              content = content.replace(/{{.*}}/, '')
+            }
+          }
+
+          return content
+        }
+      },
+      connectedCallback: {
+        value: function connectedCallback(): void {
+          this.component = component
+          if (this.attributes.length > 0) {
+            console.log(this.getAttribute(this.attributes[0].name))
+          }
+          let templateKeys = this.getTemplateKey(this.textContent)
+          this.textContent = this.renderText(this.textContent, templateKeys, component.data)
+          console.log('kkkkkk')
+        }
+      }
+    })
+
+    customElements.define(`${component.is}`, DiliElement)
   }
 }
