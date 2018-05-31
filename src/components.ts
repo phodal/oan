267e9dca5553d1e88510f709ae29910039213cac
Phodal HUANG <h@phodal.com>
2018-05-31 22:22:01 +0800

    [T] add some vue examples

--- src/components.ts
+++ src/components.ts
@@ -13,24 +13,30 @@ export const DiliComponent = function(component: any) {
       value: function constructor() {
         this.component = component
         this.data = Object.assign({}, component.data)
+        let data = this.data
 
-        Observe(this.data, this)
+        Observe(data, this)
 
         this.childNodes.forEach((node: any) => {
           let reg = /{{(.*)}}/
-          let componentKey = /\[(.*)\]/
-
+          let that = this
           if (node.tagName === 'INPUT') {
             if (node.attributes && node.attributes.length > 0) {
+              let mapName
               for (let i = 0; i < node.attributes.length; i++) {
                 let attr = node.attributes[i]
-                if (componentKey.test(attr.name)) {
-                  let keyName = RegExp.$1
-                  if (keyName === 'model') {
-                    let watcher = new Watcher(this, node, attr.value, 'input')
-                  }
+                if (attr.name === '[model]') {
+                  let name = (mapName = attr.nodeValue)
+                  node.addEventListener('input', function(e: any) {
+                    that.data[name] = e.target.value
+                  })
+
+                  node.value = that.data[name]
+                  node.removeAttribute('[model]')
                 }
               }
+              console.log(this, node, mapName)
+              let watcher = new Watcher(this, node, mapName, 'input')
             }
           }
 
@@ -38,13 +44,14 @@ export const DiliComponent = function(component: any) {
             if (reg.test(node.nodeValue)) {
               let name = RegExp.$1 // 获取匹配到的字符串
               name = name.trim()
+              console.log(name, this, node, name)
               let watcher = new Watcher(this, node, name, 'text')
             }
           }
         })
 
         setTimeout(() => {
-          this.data.text2 = 'Hello...'
+          this.data.text = 'Hello...'
         }, 50)
 
         return DiliElement
